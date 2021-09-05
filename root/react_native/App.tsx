966f82b3bf1d0ff37528cd2a04eca1c9a1b07d62
kaji46 <naotokajimura@gmail.com>
2021-09-05 23:09:31 +0900

    20210905

--- root/react_native/App.tsx
+++ root/react_native/App.tsx
@@ -1,12 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
+import { Image, Text, Input } from "react-native-elements";
 
 export default function App() {
+  const onPress = () => {
+    // 観光地の詳細ページへ遷移
+  };
   return (
     <View style={styles.container}>
-      <Text>test </Text>
-      <StatusBar style="auto" />
+      <Input placeholder="行き先はどちらですか？" />
+
+      <Text h1>今日の旅行先を選択</Text>
+
+      <Pressable onPress={onPress}>
+        <Text h2>沖縄県</Text>
+        <Image
+          source={{
+            uri: "https://cookbiz.jp/soken/core/wp-content/uploads/2019/09/Fotolia_225618711_S.jpg",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </Pressable>
     </View>
   );
 }
