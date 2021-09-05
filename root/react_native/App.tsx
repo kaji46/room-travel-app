c2de73d03accbdb33432366cedb1002d845ecc9a
kaji46 <naotokajimura@gmail.com>
2021-09-05 22:01:31 +0900

    test

--- root/react_native/App.tsx
+++ root/react_native/App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+      <Text>test </Text>
       <StatusBar style="auto" />
     </View>
   );
